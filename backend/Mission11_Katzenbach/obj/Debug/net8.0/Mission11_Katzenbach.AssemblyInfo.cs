7b6876b4367141a14b66c62f168d3ed7df5f4164
Zach Katzenbach <zachkatzenbach@gmail.com>
2025-04-01 23:33:47 -0600

    database change

--- backend/Mission11_Katzenbach/obj/Debug/net8.0/Mission11_Katzenbach.AssemblyInfo.cs
+++ backend/Mission11_Katzenbach/obj/Debug/net8.0/Mission11_Katzenbach.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Katzenbach")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+948bea1d364f6e6c2e094b4364c69ba6774e6807")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+361b46ebca902ad336ea5a427c6ea08b7e8d047f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Katzenbach")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Katzenbach")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
